ed1df0f7c8b2b08929680d882418fb6cbd5c4056
pete-wiley <46689716+pete-wiley@users.noreply.github.com>
2019-03-07 15:20:28 -0600

    Scraped data

---
+++ scraped-data.js
@@ -0,0 +1,61 @@
+[ { title: ' MR101 Intro to Quadcopters, Multi-rotors and Drones ',
+    date: ' Sat, Jan 19 2:00 PM ',
+    price: '$15',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54471078%2F208415898903%2F1%2Foriginal.20190101-160204?h=230&w=460&auto=compress&rect=0%2C0%2C2400%2C1200&s=8c307b8208a391c65e649a661d878698',
+    moreInfo:
+     'https://www.eventbrite.com/e/mr101-intro-to-quadcopters-multi-rotors-and-drones-tickets-54260075346' },
+
+
+  { title: ' Intro to Welding & Plasma Cutting October 2018 ',
+    date: ' Sat, Oct 6 1:00 PM ',
+    price: '$75',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F49458080%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C41%2C450%2C225&s=b4979a049cc0b63c252a48451b34a374',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-welding-plasma-cutting-october-2018-tickets-49973536192' },
+
+
+  { title: ' Intro to Soldering September 2018 ',
+    date: ' Sun, Sep 23 2:00 PM ',
+    price: '$45',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F49457938%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C24%2C596%2C298&s=f4575da8e5485cff9d7afb18507aea36',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-soldering-september-2018-tickets-49973472000' },
+
+
+  { title: ' Intro to Welding & Plasma Cutting ',
+    date: ' Sat, Sep 1 1:00 PM ',
+    price: '$75',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F48534823%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C41%2C450%2C225&s=a1e544190c87e3da6100a4607a6e7deb',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-welding-plasma-cutting-tickets-49202841022' },
+
+
+  { title: ' Intro to Soldering August 2018 ',
+    date: ' Sat, Aug 25 1:00 PM ',
+    price: '$10 - $45',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F48533637%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C24%2C596%2C298&s=cd95bb065dc0c75eb57c4b7e87abf28d',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-soldering-august-2018-tickets-49201760791' },
+
+
+  { title: ' Intro to Soldering May 2018 ',
+    date: ' Sat, May 19 9:30 AM ',
+    price: '$20 - $50',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F43808014%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C24%2C596%2C298&s=0d2ffbbcf2b71e726f66b3e971717bcd',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-soldering-may-2018-tickets-45366677955' },
+
+     
+  { title: ' Intro to Soldering ',
+    date: ' Sat, Feb 10 2:00 PM ',
+    price: '$20 - $50',
+    image:
+     'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F38768423%2F208415898903%2F1%2Foriginal.jpg?h=230&w=460&auto=compress&rect=0%2C24%2C596%2C298&s=294835c040ed9c2b5d9f4446c80673c1',
+    moreInfo:
+     'https://www.eventbrite.com/e/intro-to-soldering-tickets-33916578399' } ]
